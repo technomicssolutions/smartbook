3148f5c534cf93f91c48330eb5200870aa2d00e8
technomics <technomics@technomics-desktop.(none)>
2014-03-07 09:44:26 +0530

    urls added

---
+++ smartbook/expenses/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+
+from web.views import *
+
+urlpatterns = patterns('',
+
+)
--- smartbook/smartbook/urls.py
+++ smartbook/smartbook/urls.py
@@ -5,4 +5,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^admin/', include(admin.site.urls)),
+	url(r'', include('web.urls')),
+	url(r'^purchase/', include('purchase.urls')),
+	url(r'^sales/', include('sales.urls')),
+	url(r'^inventory/', include('inventory.urls')),
+	url(r'^reports/', include('reports.urls')),
+	url(r'^expenses/', include('expenses.urls')),
 )
