4600935fbf9ebb20b7f8ea60e5c427f1dd31ac24
sreedevi <sreedevi@technomicssolutions.com>
2014-03-07 19:35:28 +0530

    issue solved

--- smartbook/web/admin.py
+++ smartbook/web/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from web.models import *
 
-
-
-admin.site.register(ContactUs)
 admin.site.register(UserProfile)
 admin.site.register(Vendor)
 admin.site.register(Customer)
