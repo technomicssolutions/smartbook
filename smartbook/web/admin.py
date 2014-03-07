8710fddf3eb065cd4ec30ae2b26976401990b15d
sreedevi <sreedevi@technomicssolutions.com>
2014-03-07 19:19:44 +0530

    New

---
+++ smartbook/web/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from web.models import *
+
+
+
+admin.site.register(ContactUs)
+admin.site.register(UserProfile)
+admin.site.register(Vendor)
+admin.site.register(Customer)
+admin.site.register(Staff)
+
+
+
