ae9ab76e7edf4b5966f016b1a72afec15b4b00ea
anuj <anuj@technomicssolutions.com>
2014-03-09 15:18:39 +0530

    added edit html

--- smartbook/inventory/views.py
+++ smartbook/inventory/views.py
@@ -52,6 +52,7 @@ class ItemAdd(View):
 class ItemList(View):
 	def get(self, request, *args, **kwargs):
 		items = Item.objects.all()
+
 		ctx = {
 			'items':items,
 		}
@@ -63,8 +64,12 @@ class ItemEdit(View):
 		print "item ",kwargs['item_id']
 		items = Item.objects.get(id = kwargs['item_id'])
 		print "items",items.name
+		brand = Brand.objects.all()
+		uom = UnitOfMeasure.objects.all()
 		ctx ={
 			'items':items,
+			'uoms': uom,
+			'brands': brand,
 		}
 		return render(request, 'inventory/edit_item.html',ctx)
 
