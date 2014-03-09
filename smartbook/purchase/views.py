a72bf0d1b73de122e029353dc69566adcfab06a9
ansal <ansalcnasar@gmail.com>
2014-03-09 23:53:57 +0530

    latest purchase

--- smartbook/purchase/views.py
+++ smartbook/purchase/views.py
@@ -9,10 +9,19 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+from inventory.models import Item
+from inventory.models import UnitOfMeasure
+from inventory.models import Brand
 
 from web.models import (UserProfile,Vendor,Customer,Staff)
 
 class PurchaseEntry(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'purchase/purchase_entry.html',{})
+    	brand = Brand.objects.all()
+    	vendor = Vendor.objects.all()
+
+        return render(request, 'purchase/purchase_entry.html',{
+        	'brands' : brand,
+        	'vendors' : vendor,
+        	})
         
