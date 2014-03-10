217b9515f28197014abb28b6e3c6bc5dd8d8dcb1
technomics <technomics@technomics-desktop.(none)>
2014-03-10 11:07:49 +0530

    invetory migration

--- smartbook/inventory/migrations/0013_auto__add_unique_item_code__add_unique_item_name.py
+++ smartbook/inventory/migrations/0013_auto__add_unique_item_code__add_unique_item_name.py
@@ -8,39 +8,23 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding field 'Item.brand'
-        db.add_column(u'inventory_item', 'brand',
-                      self.gf('django.db.models.fields.related.ForeignKey')(default=1, to=orm['inventory.Brand']),
-                      keep_default=False)
+        # Adding unique constraint on 'Item', fields ['code']
+        db.create_unique(u'inventory_item', ['code'])
 
-        # Deleting field 'Brand.description'
-        db.delete_column(u'inventory_brand', 'description')
-
-        # Deleting field 'Brand.name'
-        db.delete_column(u'inventory_brand', 'name')
-
-        # Adding field 'Brand.brand'
-        db.add_column(u'inventory_brand', 'brand',
-                      self.gf('django.db.models.fields.CharField')(default=1, max_length=50),
-                      keep_default=False)
+        # Adding unique constraint on 'Item', fields ['name']
+        db.create_unique(u'inventory_item', ['name'])
 
     def backwards(self, orm):
-        # Deleting field 'Item.brand'
-        db.delete_column(u'inventory_item', 'brand_id')
+        # Removing unique constraint on 'Item', fields ['name']
+        db.delete_unique(u'inventory_item', ['name'])
 
-
-        # User chose to not deal with backwards NULL issues for 'Brand.description'
-        raise RuntimeError("Cannot reverse this migration. 'Brand.description' and its values cannot be restored.")
-
-        # User chose to not deal with backwards NULL issues for 'Brand.name'
-        raise RuntimeError("Cannot reverse this migration. 'Brand.name' and its values cannot be restored.")
-        # Deleting field 'Brand.brand'
-        db.delete_column(u'inventory_brand', 'brand')
+        # Removing unique constraint on 'Item', fields ['code']
+        db.delete_unique(u'inventory_item', ['code'])
 
     models = {
         u'inventory.brand': {
             'Meta': {'object_name': 'Brand'},
-            'brand': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'brand': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '51'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
         },
         u'inventory.inventory': {
@@ -56,11 +40,12 @@ class Migration(SchemaMigration):
             'Meta': {'object_name': 'Item'},
             'barcode': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
             'brand': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['inventory.Brand']"}),
-            'code': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '10'}),
             'description': ('django.db.models.fields.TextField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'tax': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '14', 'decimal_places': '2'})
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            'tax': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '14', 'decimal_places': '2'}),
+            'uom': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['inventory.UnitOfMeasure']"})
         },
         u'inventory.unitofmeasure': {
             'Meta': {'object_name': 'UnitOfMeasure'},
