27967ec03a4f2d3fc2a1a221c1b8a97e616c0132
ansal <ansalcnasar@gmail.com>
2014-03-12 12:18:54 +0530

    edit urls sales

--- smartbook/sales/urls.py
+++ smartbook/sales/urls.py
@@ -5,9 +5,9 @@ from sales.views import *
 urlpatterns = patterns('',
 #	url(r'^$', Home.as_view(), name='home'),
 
-	url(r'sales/$', Sales.as_view(), name='sales'),
-	url(r'sales_entry/$', Sales.as_view(), name='sales_entry'),
-	url(r'return_entry/$', ReturnEntry.as_view(), name='return_entry'),
+	
+	url(r'sales_entry/$', SalesEntry.as_view(), name='sales_entry'),
+	url(r'return_entry/$', SalesReturn.as_view(), name='return_entry'),
 	url(r'view_sales/$', ViewSales.as_view(), name='view_sales'),
 
 
--- smartbook/sales/views.py
+++ smartbook/sales/views.py
@@ -2,7 +2,7 @@
 import sys
 
 from django.db import IntegrityError
-
+from django.db.models import Max
 from django.contrib.auth.views import password_reset
 from django.shortcuts import get_object_or_404, render
 from django.views.generic.base import View
@@ -10,16 +10,23 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+from sales.models import Sales
 
 #from sales.models import *
 
-class Sales(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'sales/sales_entry.html',{})
+class SalesEntry(View):
+	def get(self, request, *args, **kwargs):
+		sales_invoice_number = Sales.objects.aggregate(Max('sales_invoice_number'))['sales_invoice_number__max']
+		if not sales_invoice_number:
+			sales_invoice_number = 1
+		return render(request, 'sales/sales_entry.html',{
+			'sales_invoice_number': sales_invoice_number,
+    	})
+    
 
-class ReturnEntry(View):
+class SalesReturn(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'sales/return_entry.html',{})
+        return render(request, 'sales/sales_return.html',{})
 
 class ViewSales(View):
     def get(self, request, *args, **kwargs):
