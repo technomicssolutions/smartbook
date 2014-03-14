38c3f752315824a40ae4c4e1d9ceacc56958f9c4
Geethu <geethu@geethu-Lenovo-G570.(none)>
2014-03-14 16:01:28 +0530

    Added userid to url

--- smartbook/web/models.py
+++ smartbook/web/models.py
@@ -3,102 +3,123 @@ from django.contrib.auth.models import User
 from django.conf import settings
 
 USER_TYPE = (
-	('vendor', 'Vendor'),
-	('customer', 'Customer'),
-	('staff','Staff')
+    ('vendor', 'Vendor'),
+    ('customer', 'Customer'),
+    ('staff','Staff')
 )
 
 class Designation(models.Model):
 
-	title = models.CharField('Designation title', max_length=50, unique=True)
-	description = models.CharField('Description', max_length=100, null=True, blank=True)
+    title = models.CharField('Designation title', max_length=50, unique=True)
+    description = models.CharField('Description', max_length=100, null=True, blank=True)
 
-	def __unicode__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title
 
-	class Meta:
-		verbose_name = 'Designation'
-		verbose_name_plural = 'Designation'
+    class Meta:
+        verbose_name = 'Designation'
+        verbose_name_plural = 'Designation'
 
 class UserProfile(models.Model):
-	user = models.ForeignKey(User)
-	user_type = models.CharField('User Type', max_length=10, choices=USER_TYPE)
-	house_name = models.CharField('House name', null=True, blank=True, max_length=50)
-	street = models.CharField('Street', null=True, blank=True, max_length=50)
-	city = models.CharField('City', null=True, blank=True, max_length=50)
-	district = models.CharField('District', null=True, blank=True, max_length=50)
-	pin = models.CharField('Pin', max_length=10, null=True, blank=True,)
-	mobile = models.CharField('Mobile', max_length=15, null=True, blank=True)
-	land_line = models.CharField('Land Line',max_length=15, blank=True)
-	email_id = models.CharField('Email Id', max_length=30)
+    user = models.ForeignKey(User)
+    user_type = models.CharField('User Type', max_length=10, choices=USER_TYPE)
+    house_name = models.CharField('House name', null=True, blank=True, max_length=50)
+    street = models.CharField('Street', null=True, blank=True, max_length=50)
+    city = models.CharField('City', null=True, blank=True, max_length=50)
+    district = models.CharField('District', null=True, blank=True, max_length=50)
+    pin = models.CharField('Pin', max_length=10, null=True, blank=True,)
+    mobile = models.CharField('Mobile', max_length=15, null=True, blank=True)
+    land_line = models.CharField('Land Line',max_length=15, blank=True)
+    email_id = models.CharField('Email Id', max_length=30)
 
-	def __unicode__(self):
-		return self.user.username
+    def __unicode__(self):
+        return self.user.username
 
-	class Meta:
-		verbose_name = 'User Profile'
-		verbose_name_plural = 'User Profile'
+    class Meta:
+        verbose_name = 'User Profile'
+        verbose_name_plural = 'User Profile'
 
 class Vendor(models.Model):
-	user = models.ForeignKey(User)
-	contact_person = models.CharField('Contact Person', max_length=50)	
+    user = models.ForeignKey(User)
+    contact_person = models.CharField('Contact Person', max_length=50)  
 
-	def __unicode__(self):
-		return "vendor - "+self.user.first_name
+    def save(self, *args, **kwargs):
+        profile = self.user.userprofile_set.all()
+        if len(profile) == 0:
+            profile = UserProfile.objects.create(user = self.user)
+        super(Vendor, self).save(*args, **kwargs)
 
-	class Meta:
+    def __unicode__(self):
+        return "vendor - "+self.user.first_name
+    
 
-		verbose_name = 'Vendor'
-		verbose_name_plural = 'Vendor'
+    class Meta:
 
-	
+        verbose_name = 'Vendor'
+        verbose_name_plural = 'Vendor'
+
+    
 class Customer(models.Model):
-	user = models.ForeignKey(User)
+    user = models.ForeignKey(User)
 
-	def __unicode__(self):
-		return "customer - "+self.user.first_name
+    def __unicode__(self):
+        return "customer - "+self.user.first_name
 
-	class Meta:
+    def save(self, *args, **kwargs):
+        profile = self.user.userprofile_set.all()
+        if len(profile) == 0:
+            profile = UserProfile.objects.create(user = self.user)
+        super(Customer, self).save(*args, **kwargs)
 
-		verbose_name = 'Customer'
-		verbose_name_plural = 'Customer'
 
-		
+    class Meta:
+
+        verbose_name = 'Customer'
+        verbose_name_plural = 'Customer'
+
+
+        
 class Staff(models.Model):
-	user = models.ForeignKey(User)
-	designation = models.ForeignKey(Designation, null=True, blank=True)
+    user = models.ForeignKey(User)
+    designation = models.ForeignKey(Designation, null=True, blank=True)
 
-	def __unicode__(self):
-		return "staff - "+self.user.first_name
+    def __unicode__(self):
+        return "staff - "+self.user.first_name
 
-	class Meta:
+    def save(self, *args, **kwargs):
+        profile = self.user.userprofile_set.all()
+        if len(profile) == 0:
+            profile = UserProfile.objects.create(user = self.user)
+        super(Staff, self).save(*args, **kwargs)
 
-		verbose_name = 'Staff'
-		verbose_name_plural = 'Staff'
+    class Meta:
+
+        verbose_name = 'Staff'
+        verbose_name_plural = 'Staff'
 
 class TransportationCompany(models.Model):
 
-	company_name = models.CharField('Company Name', null=True, blank=True, max_length=50)
+    company_name = models.CharField('Company Name', null=True, blank=True, max_length=50)
 
-	def __unicode__(self):
+    def __unicode__(self):
 
-		return self.company_name
+        return self.company_name
 
-	class Meta:
+    class Meta:
 
-		verbose_name = 'Transportation Company'
-		verbose_name_plural = 'Transportation Company'
+        verbose_name = 'Transportation Company'
+        verbose_name_plural = 'Transportation Company'
 
 class OwnerCompany(models.Model):
 
-	company_name = models.CharField('Company Name', max_length=100)
+    company_name = models.CharField('Company Name', max_length=100)
 
-	def __unicode__(self):
+    def __unicode__(self):
 
-		return self.company_name
+        return self.company_name
 
-	class Meta:
+    class Meta:
 
-		verbose_name = 'Owner Company'
-		verbose_name_plural = 'Owner Company'
+        verbose_name = 'Owner Company'
+        verbose_name_plural = 'Owner Company'
 
