98c7a8995b90c8f6ccb0cb2a987208f9a1d3c227
geethu <geethu@technomicssolutions.com>
2014-05-30 11:41:53 +0530

    Removed migrations and corrected the models in web

--- smartbook/sales/models.py
+++ smartbook/sales/models.py
@@ -3,9 +3,8 @@ from django.contrib.auth.models import User
 
 from datetime import datetime
 
-from inventory.models import Item
-from inventory.models import Brand
-from web.models import Customer
+from inventory.models import Item, Brand
+from web.models import Customer, UserProfile
 
 
 # Create your models here. 
@@ -13,6 +12,7 @@ from web.models import Customer
 PAYMENT_MODE = (
     ('cash', 'Cash'),
     ('cheque', 'Cheque'),
+    ('credit', 'Credit'),
 )
 
 class Quotation(models.Model):
@@ -62,13 +62,12 @@ class QuotationItem(models.Model):
 class DeliveryNote(models.Model):
 
     quotation = models.ForeignKey(Quotation, null=True, blank=True)
-    customer = models.ForeignKey(Customer, null=True, blank=True)
-    delivery_note_number = models.CharField('Delivery Note Serial number', max_length=50, null=True, blank=True)
+    salesman = models.ForeignKey(User, null=True, blank=True)
+    delivery_note_number = models.CharField('Delivery Note Serial number', max_length=50, null=True, blank=True, unique=True)
     date = models.DateField('Date', null=True, blank=True)
     lpo_number = models.CharField('LPO Number', null=True, blank=True, max_length=20)
-    prefix = models.CharField('Prefix', null=True, blank=True, max_length=20, default='DN')
-    processed = models.BooleanField('Is Processed', default=False)
     net_total = models.DecimalField('Net Total',max_digits=14, decimal_places=2, default=0)
+    is_pending = models.BooleanField('Pending items', default=True)
 
 
     def __unicode__(self):
@@ -88,6 +87,9 @@ class DeliveryNoteItem(models.Model):
     net_amount = models.DecimalField('Net Amount', max_digits=14, decimal_places=2, default=0)
     quantity_sold = models.IntegerField('Quantity Sold', default=0)
     discount = models.DecimalField('Total Discount', max_digits=14, decimal_places=2, default=0)
+    total_quantity = models.IntegerField('Total Quantity', default=0)
+    is_completed = models.BooleanField('Items sold completely', default=False)
+    selling_price = models.DecimalField('Selling Price', max_digits=14, decimal_places=2, default=0) 
 
     def __unicode__(self):
 
@@ -101,7 +103,7 @@ class DeliveryNoteItem(models.Model):
 
 class Sales(models.Model): 
 
-    sales_invoice_number = models.CharField('Sales Invoice Number', null=True, blank=True, max_length=10)
+    sales_invoice_number = models.CharField('Sales Invoice Number', null=True, blank=True, max_length=10, unique=True)
     sales_invoice_date = models.DateField('Sales Invoice Date', null=True, blank=True)
     customer = models.ForeignKey(Customer, null=True, blank=True)
     salesman = models.ForeignKey(User, null=True, blank=True)
@@ -111,11 +113,14 @@ class Sales(models.Model):
     net_amount = models.DecimalField('Net Amount',max_digits=14, decimal_places=2, default=0)
     round_off = models.DecimalField('Net Round Off',max_digits=14, decimal_places=2, default=0)
     grant_total = models.DecimalField('Grand Total',max_digits=14, decimal_places=2, default=0)
-    discount = models.DecimalField('Total Discount',max_digits=14, decimal_places=2, default=0)
-    quotation = models.ForeignKey(Quotation, null=True, blank=True)
+    discount = models.DecimalField('Item wise Discount',max_digits=14, decimal_places=2, default=0)
     delivery_note = models.ForeignKey(DeliveryNote, null=True, blank=True)
     lpo_number = models.CharField('LPO Number', null=True, blank=True, max_length=30)
-
+    is_processed = models.BooleanField('Processed', default=False)
+    balance = models.DecimalField('Balance', null=True, blank=True, decimal_places=2, default=0, max_digits=14)
+    paid = models.DecimalField('Paid', null=True, blank=True, decimal_places=2, default=0, max_digits=14)
+    discount_for_sale = models.DecimalField('Discount for sale',max_digits=14, decimal_places=2, default=0)
+    discount_percentage_for_sale = models.DecimalField('Discount percentage for sale',max_digits=14, decimal_places=2, default=0)
     def __unicode__(self):
 
         return str(self.sales_invoice_number)
@@ -125,34 +130,13 @@ class Sales(models.Model):
         verbose_name = 'Sales'
         verbose_name_plural = 'Sales'
 
-
-class SalesInvoice(models.Model):
-
-    quotation = models.ForeignKey(Quotation, null=True, blank=True)
-    delivery_note = models.ForeignKey(DeliveryNote, null=True, blank=True)
-    sales = models.ForeignKey(Sales)
-    customer = models.ForeignKey(Customer, null=True, blank=True)
-    date = models.DateField('Date', null=True, blank=True)
-    invoice_no = models.CharField('Invoice Number',null=True, blank=True, max_length=20)
-    prefix =  models.CharField('Prefix', max_length=20, default='INV')
-    is_processed = models.BooleanField('Processed', default=False)
-
-
-    def __unicode__(self):
-
-        return str(self.invoice_no)
-
-    class Meta:
-
-        verbose_name = 'Sales Invoice'
-        verbose_name_plural = 'Sales Invoice'
-
 class SalesItem(models.Model):
 
     item = models.ForeignKey(Item)
     sales = models.ForeignKey(Sales)
     quantity_sold = models.IntegerField('Quantity Sold', default=0)
-    discount_given = models.DecimalField('Discount Given',max_digits=14, decimal_places=2, default=0)  
+    discount_amount = models.DecimalField('Discount amount',max_digits=14, decimal_places=2, default=0)  
+    discount_percentage = models.DecimalField('Discount percentage',max_digits=14, decimal_places=2, default=0) 
     selling_price = models.DecimalField('Selling Price', max_digits=14, decimal_places=2, default=0) 
     net_amount = models.DecimalField('Sold Net Amount', max_digits=14, decimal_places=2, default=0)
     
@@ -170,7 +154,7 @@ class SalesReturn(models.Model):
     sales = models.ForeignKey(Sales)
     return_invoice_number = models.IntegerField('Sales Return invoice number', unique=True)
     date = models.DateField('Date', null=True, blank=True)
-    net_amount = models.IntegerField('Total', null=True, blank=True)
+    net_amount = models.DecimalField('Total', max_digits=14, decimal_places=2, default=0)
     
 
     def __unicode__(self):
@@ -180,7 +164,7 @@ class SalesReturnItem(models.Model):
     sales_return = models.ForeignKey(SalesReturn, null=True, blank=True)
     item = models.ForeignKey(Item, null=True, blank=True)
     return_quantity = models.IntegerField('Return Quantity', null=True, blank=True)
-    amount = models.IntegerField('Amount', null=True, blank=True)
+    amount = models.DecimalField('Amount', max_digits=14, decimal_places=2, default=0)
 
     def __unicode__(self):
 
@@ -188,33 +172,40 @@ class SalesReturnItem(models.Model):
 
 class ReceiptVoucher(models.Model):
 
-    sales_invoice = models.ForeignKey(SalesInvoice, null=True, blank=True)
-    receipt_voucher_no = models.CharField('Receipt Voucher No', null=True, blank=True, max_length=30)
+    sales_invoice = models.ForeignKey(Sales, null=True, blank=True)
+    receipt_voucher_no = models.CharField('Receipt Voucher No', null=True, blank=True, max_length=30, unique=True)
     date = models.DateField('Date', null=True, blank=True)
-    customer = models.ForeignKey(Customer, null=True, blank=True)
-    sum_of = models.DecimalField('Sum of', max_digits=14, decimal_places=2, default=0)
+    total_amount = models.DecimalField('Total Amount', max_digits=14, decimal_places=2, default=0)
+    paid_amount = models.DecimalField('Paid Amount', max_digits=14, decimal_places=2, default=0)
     cheque_no = models.CharField('Check Number', null=True, blank=True, max_length=50)
     bank = models.CharField('Bank', null=True, blank=True, max_length=100)
     dated = models.DateField('Dated', null=True, blank=True)
     payment_mode = models.CharField('Payment Mode', null=True, blank=True, max_length=40, choices=PAYMENT_MODE)
-    prefix = models.CharField('Prefix', null=True, blank=True, max_length=20, default='RV')
-
+    
     def __unicode__(self):
 
-        return str(self.sales_invoice.invoice_no)
+        return str(self.sales_invoice.sales_invoice_number)
 
     class Meta:
 
         verbose_name = 'Receipt Voucher'
         verbose_name_plural = 'Receipt Voucher'
 
+class CustomerAccount(models.Model):
+
+    invoice_no = models.ForeignKey(Sales, null=True, blank=True)
+    customer = models.ForeignKey(Customer, null=True, blank=True)
+    total_amount = models.DecimalField('Total amount', max_digits=14, decimal_places=2, default=0)
+    paid = models.DecimalField('Paid', max_digits=14, decimal_places=2, default=0)
+    balance = models.DecimalField('Balance', max_digits=14, decimal_places=2, default=0)
+    is_complted = models.BooleanField('Is Completed', default=False)
 
 
+    class Meta:
 
+        verbose_name = 'Customer Account'
+        verbose_name_plural = 'Customer Account'
 
+    def __unicode__(self):
 
-
-
-        
-
-
+        return str(self.invoice_no.sales_invoice_number)
